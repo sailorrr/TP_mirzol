9c3df1e3232efb90a452f3b14083c0f55a64470a
mirzol <05ru10189@gmail.com>
2026-09-24 20:12:43 +0300

    Add .gitignore, remove build/ from tracking

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_lab-crypto_FILE C:/Users/adminn/lab-crypto/build/lab-crypto.exe)
-set(__QT_DEPLOY_TARGET_lab-crypto_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_lab-crypto_RUNTIME_DLLS C:/Qt/6.11.2/mingw_64/bin/Qt6Core.dll)
